2f1185e1759956d16d5e9652951b49e39d463981
Linhchi162 <linhchi1622k4@gmail.com>
2026-04-29 18:47:37 +0700

    set DQN sim

--- retrain_dqn.sh
+++ retrain_dqn.sh
@@ -44,7 +44,7 @@ for seed in "${SEEDS[@]}"; do
             --reward_type "$reward" \
             --seed "$seed" \
             --total_steps $STEPS \
-            --sim_duration 7200 \
+            --sim_duration 100000 \
             --n_envs $N_ENVS \
             --buffer_size 500000 \
             --save_dir "$sd" \
